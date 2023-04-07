8d657a8b3deace0247e35e186ba9a934da88b650
xujia.fu2 <xujia.fu2@envision-digital.com>
2023-04-07 17:31:20 +0800

    add federation sample code

---
+++ datafederation/GetDownloadStatus.java
@@ -0,0 +1,32 @@
+package com.envisioniot.example.sample.datafederation;
+
+import com.alibaba.fastjson.JSONObject;
+import com.envision.apim.poseidon.config.PConfig;
+import com.envision.apim.poseidon.core.Poseidon;
+import com.envisioniot.example.sample.utility.Request;
+
+public class GetDownloadStatus {
+    public JSONObject getDownloadStatus(
+            String accessKey,
+            String secretKey,
+            String orgId,
+            String url,
+            String channelId,
+            String taskId)
+    {
+        Request request = new Request();
+        request.setQueryParam("orgId", orgId);
+        request.setMethod("GET");
+
+        try {
+            JSONObject response = Poseidon.config(PConfig.init().appKey(accessKey).appSecret(secretKey))
+                    .url(url + "/data-federation/v2.0/channels/read/" + channelId + "/download/" + taskId + "/status")
+                    .getResponse(request, JSONObject.class);
+            System.out.println(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
---
+++ datafederation/ListChannels.java
@@ -0,0 +1,40 @@
+package com.envisioniot.example.sample.datafederation;
+
+import com.alibaba.fastjson.JSONObject;
+import com.envision.apim.poseidon.config.PConfig;
+import com.envision.apim.poseidon.core.Poseidon;
+import com.envisioniot.example.sample.utility.Request;
+
+//https://www.envisioniot.com/docs/data-federation-api/en/2.3.0/list_channels.html
+
+public class ListChannels {
+    public JSONObject listChannels(
+            String accessKey,
+            String secretKey,
+            String orgId,
+            String url,
+            String channelType)
+    {
+        Poseidon poseidon = Poseidon.config(
+                PConfig.init()
+                        .appKey(accessKey)
+                        .appSecret(secretKey)
+        ).method("GET").header("Content-Type", "application/json");
+
+        Request request = new Request();
+        request.setQueryParam("orgId", orgId);
+        request.setQueryParam("channelType", channelType);
+
+        try {
+            JSONObject response = poseidon
+                    .url(url + "/data-federation/v2.0/channels")
+                    .getResponse(request, JSONObject.class);
+            System.out.println(response);
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
